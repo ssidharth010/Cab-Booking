f03ad2f2e3008f59861622d15d6fe2d98721e4b6
ssidharth010 <ssidharth010@gmail.com>
2023-12-31 22:06:08 +0530

    Feat: Create form creater and viewer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,33 @@
-import Header from "@/components/header/page";
-import {ClerkProvider} from "@clerk/nextjs";
-import './globals.css'
-import { Metadata } from "next";
+import DesignerContextProvider from "@/components/context/DesignerContext";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Form Builder',
-  description: 'Create forms easily',
-}
+  title: "Form Builder",
+  description: "Create and share forms",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark">
-      <body className="container bg-background">
-      <ClerkProvider>
-        <Header />
-        {children}
-      </ClerkProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <NextTopLoader />
+          <DesignerContextProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </DesignerContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
